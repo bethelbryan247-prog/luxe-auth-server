1a32824bac2660d4bd0293a806bd8ee2da698f1d
bethelbryan247-prog <bethelbryan247@gmail.com>
2026-07-30 14:54:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -66,12 +66,14 @@
   const otpStore = new Map();
 
   const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-    }
-  });
+  }
+});
 
   // Send OTP
 app.post('/api/send-otp', async (req, res) => {
